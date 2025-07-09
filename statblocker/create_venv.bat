76b905ff061f5030f3d615cb742776b42d89efea
Jonathan Bailey <jonathan.bailey@comcast.net>
2025-07-09 02:10:58 -0400

    fix script

--- statblocker/create_venv.bat
+++ statblocker/create_venv.bat
@@ -1,5 +1,16 @@
+@echo off
+
+REM Create virtual environment in statblocker/_venv
 python -m venv _venv
-cd _venv/Scripts
-python.exe -m pip install --upgrade pip
-python.exe -m pip install -r requirements.txt
-cd ../../
+
+REM Upgrade pip in the venv
+_venv\Scripts\python.exe -m pip install --upgrade pip
+
+REM Install the project from the parent directory (where pyproject.toml is)
+_venv\Scripts\python.exe -m pip install ..
+_venv\Scripts\python.exe -m pip install "..[dev]"
+
+REM Optionally activate the virtual environment (for interactive use)
+call _venv\Scripts\activate.bat
+
+echo Virtual Environment setup complete!
